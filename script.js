6e77398b81945156451c7c1673bf01e797793b97
Katelyn <kcm34@pitt.edu>
2023-04-12 15:24:16 -0700

    added event listener with hover element to divs to change color

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-//Create a 16x16 grid of square divs using JavaScript
-//Grid squares inside another container div, which can go directly into the HTML file
-//divs should appear as a grid; utilize flexbox 
-
 const grid_container = document.getElementById("grid_container");
 
 
+//function creates a 16x16 grid by creating new divs and manipulating the DOM
 function createGrid() {
 
+    //creates 16 column divs
     for (let i = 0; i < 16; i++) {
-        
         const column = document.createElement("div");
 
         //creates a column of 16 divs showns as squares
         for (let j = 0; j < 16; j++) {
-
             const square = document.createElement('div');
             square.setAttribute("class", "square");
-            //adds a node to the end of the list of children of specified parent node
             column.appendChild(square);
         }
- 
+
         column.setAttribute("class", "column");
         grid_container.appendChild(column); 
-
     }
 }
 
 createGrid();
+
+//function that adds an event to each div and changes the color 
+function changeColors() {
+
+    const squares = document.querySelectorAll('div')
+
+    //This script will allow multiple event listeners 
+    squares.forEach(div => {
+        div.addEventListener("mouseover", function() {
+                div.style.backgroundColor = "black";
+            });
+        });
+
+}
+
+changeColors();
