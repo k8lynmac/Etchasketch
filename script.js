006a9ff614d9c59e3d744c9f9992342377b0a40f
Katelyn <kcm34@pitt.edu>
2023-04-17 15:10:19 -0700

    fixed container with changing width sizes

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
 const grid_container = document.getElementById("grid_container");
 const width = 16;
 createGrid(width);
-changeColors();
 getDimensions();
 
-
-//function creates a 16x16 grid by creating new divs and manipulating the DOM
+//function creates a widthxwidth grid by creating new divs and manipulating the DOM
 function createGrid(width) {
-
-    //creates 16 divs shown as columns
-    for (let i = 0; i < width; i++) {
-        const column = document.createElement("div");
-
-        //creates a column of 16 divs showns as squares
-        for (let j = 0; j < width; j++) {
-            const square = document.createElement('div');
-            square.setAttribute("class", "square");
-            column.appendChild(square);
-        }
-
-        column.setAttribute("class", "column");
-        let height = parseInt(600/width);
-        column.style.flexBasis = `${height}px`;
-        grid_container.appendChild(column); 
+    let area = width*width;
+    for (let i = 0; i < area; i++) {
+        const square = document.createElement("div");
+        grid_container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+        grid_container.style.gridTemplateRows = `repeat(${width}, 1fr)`;
+        square.setAttribute("class", "square");
+        grid_container.appendChild(square); 
+        changeColors();
     }
 }
 
 //function that adds an event to each div and changes the color 
 function changeColors() {
-
     const squares = document.querySelectorAll('.square')
 
     //This script will allow multiple event listeners 
@@ -52,16 +41,12 @@ function getDimensions() {
                 } while (width > 100);
                 removeChildren();
                 createGrid(width);
-                changeColors();
             });
         });
-    }
+ }
 
 function removeChildren() {
     while (grid_container.hasChildNodes()) {
         grid_container.removeChild(grid_container.firstChild);
     }
 }   
-    
-
- 
